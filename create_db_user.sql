92efa4b40d9183c5d5796104d6fd8dfb118f3562
CrodBac4rdi <birkner.daniel@googlemail.com>
2025-07-28 20:26:44 +0200

    Update create_db_user.sql

--- create_db_user.sql
+++ create_db_user.sql
@@ -1,4 +1,4 @@
 -- Run this as postgres superuser
 -- sudo -u postgres psql < create_db_user.sql
-CREATE USER bacardi WITH PASSWORD 'Limkahvui11!' CREATEDB;
-CREATE DATABASE showcase_dev OWNER bacardi;
+CREATE USER yourshere WITH PASSWORD 'example' CREATEDB;
+CREATE DATABASE showcase_dev OWNER yourshere;
